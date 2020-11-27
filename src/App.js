9db55a4b33544b7c4941bba1c37ded0ffc8c8c01
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2020-11-27 15:42:19 +0200

    Artist Page added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer"
+import ArtistPage from "./components/ArtistPage"
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <NavBar />
+        <Route path="/artist" exact component={ArtistPage} />
+
         <Footer />
-        
       </Router>
     );
   }
---
+++ src/components/ArtistPage.jsx
@@ -0,0 +1,237 @@
+import React from "react";
+import queen from "../assets/queen.jpg"
+
+
+class ArtistPage extends React.Component{
+    render(){
+        return(
+
+            <div class="col col-sm-12 col-md-10 col-lg-10">
+            <div class="container" id="firstContainer">
+              <div class="row">
+                <div
+                  class="col col-sm-12 col-md-12 col-lg-5 d-flex flex-wrap align-items-center justify-content-center text-center"
+                  id="imageContainer">
+                  <img class="img-fluid" src={queen} alt=""/>
+                  <h3 style={{color: "white"}}>
+                    QUEEN II (Deluxe edition 2011 Remaster)
+                  </h3>
+                  <div class="container">
+                    <div class="row">
+                      <div class="col">
+                        <small style={{color: "gray"}}>Queen</small><br />
+                        <form class="btn btn-success" style={{padding: "0"}}>
+                          <button class="btn" type="submit">Play</button>
+                        </form>
+                        <br />
+                        <small style={{color: "gray"}}>1974 | 16 SONGS</small>
+                      </div>
+                      <br />
+                      <div class="col d-flex justify-content-center form-inline">
+                        <a class="icon" href="#">
+                          <i class="fa fa-heart" style={{color: "white"}}></i></a>
+  
+                        <a class="nav-link" href="#">
+                          <i class="fa fa-ellipsis-h" style={{color: "white"}}></i></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+  
+                <div class="col col-sm-12 col-md-12 col-lg-7 justify-content-between form-inline">
+                  <div class="container">
+                    <table class="table borderless">
+                      <tbody>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            Procession - Remastered<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>1:13</td>
+                        </tr>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            Father To Son - Remastered<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>6:13</td>
+                        </tr>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            White Queen (As It Began) - Remastered<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>4:33</td>
+                        </tr>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            Some Day One Day - Remastered<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>4:22</td>
+                        </tr>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            The Loser In The End - Remastered<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>4:01</td>
+                        </tr>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            Ogre Battle - Remastered<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>4:08</td>
+                        </tr>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            The Fairy Feller's Master-Stroke - Remastered<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>2:40</td>
+                        </tr>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            Nevermore - Remastered<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>1:18</td>
+                        </tr>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            The March Of The Black Queen - Remastered<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>6:32</td>
+                        </tr>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            Funny How Love Is - Remastered<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>2:49</td>
+                        </tr>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            Seven Seas Of Rhye - Remastered<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>2:48</td>
+                        </tr>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            See What A Fool I've Been - BBC Session July 73, 2011
+                            Remix)<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>4:22</td>
+                        </tr>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            Funny How Love Is - Remastered<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>2:49</td>
+                        </tr>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            White Queen - Live at Hammersmith Dec 75<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>5:32</td>
+                        </tr>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            Seven Seas of Rhye - Instrumental Mix 2011<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>2:48</td>
+                        </tr>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            Nevermore - BBC Session, April 74<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>2:48</td>
+                        </tr>
+                        <tr>
+                          <th scope="row ">
+                            <i class="fa fa-music"></i>
+                          </th>
+                          <td style={{textalign: "left"}}>
+                            See What A Fool I've Been - B-side version Feb 74<br />
+                            <small>Queen</small>
+                          </td>
+                          <td>2:48</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+
+
+
+
+
+
+            )
+        }
+    }
+    
+    export default ArtistPage
+
